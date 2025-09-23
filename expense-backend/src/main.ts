cc6fae329c83d7974941c467d043620fe5bdcedd
Ebube Ebite <ejude44@yahoo.com>
2025-09-23 22:59:53 +0200

    feat(expense-backend): add nestjs, add prisma-postgres

--- expense-backend/src/main.ts
+++ expense-backend/src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { environment } from '../environment/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setGlobalPrefix('api');
 
-  const port = environment.port;
+  const port = process.env.PORT || 4001;
   await app.init();
 
   await app.listen(port, () => {

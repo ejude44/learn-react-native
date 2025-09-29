f578f5f7a49b527947de5e07f0f5d65616548079
Ebube Ebite <ejude44@yahoo.com>
2025-09-29 22:12:43 +0200

    chore(cloud): dont use package json

--- expense-backend/start.sh
+++ expense-backend/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "Starting application..."
-echo "DATABASE_URL exists: $([[ -n "$DATABASE_URL" ]] && echo "YES" || echo "NO")"
-echo "DATABASE_URL starts with postgresql: $([[ "$DATABASE_URL" == postgresql://* ]] && echo "YES" || echo "NO")"
 npm install --production
 npx prisma generate
 node dist/main.js
